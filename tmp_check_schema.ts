93703a55277a076de83e6a47445b027d99a4c91c
Erick Santillan <rederick2@hotmail.com>
2026-04-03 15:36:06 -0500

    Delete tmp_check_schema.ts

--- tmp_check_schema.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from './src/lib/supabase/server.ts';
-
-async function test() {
-  const supabase = await createClient();
-  const { data, error } = await supabase.from('job_expenses').select('*').limit(1);
-  console.log(JSON.stringify(data?.[0] || {}, null, 2));
-}
-
-test();
